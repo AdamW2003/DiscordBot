a498aa803c428a90f517d1f078119b5c1d2974be
AdamW2003 <adamw@river.red>
2023-08-20 00:08:44 +0100

    fixxing message kick

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,28 +1,28 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.MessageCreate,
-	async execute(interaction) {
-        const user = interaction.user;
-        const guild = interaction.guild;
+    name: Events.MessageCreate,
+    async execute(message) { // Changed 'interaction' to 'message' for consistency
+        const user = message.author; // Changed 'message.user' to 'message.author'
+        const guild = message.guild; // Changed 'interaction.guild' to 'message.guild'
 
-		console.log(`${interaction.author.tag} in #${interaction.channel.name} sent: ${interaction.content}`);
+        console.log(`${user.tag} in #${message.channel.name} sent: ${message.content}`);
 
-        if (interaction.author.bot) return;
+        if (user.bot) return;
 
-        if (user.id == '527780756643381249'){
-            await interaction.reply('blah blah blah');
-            await interaction.reply('stop typing')
+        if (user.id === '527780756643381249') {
+            await message.reply('blah blah blah');
+            await message.reply('stop typing');
 
-            const invite = await interaction.channel.createInvite({
+            const invite = await message.channel.createInvite({
                 maxAge: 86400,
                 maxUses: 1,
                 unique: true,
             });
-            
+
             await user.send(`You have been banned from ${guild.name}. Here is an invite back: ${invite.url}`);
 
             await guild.members.kick(user.id);
         }
-	},
+    },
 };
